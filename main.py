b21655542f24adc59e163b55ca3a7f14d8e14f9b
lux <dawidjasinski1778@gmail.com>
2023-03-17 08:26:06 +0100

    Dokończenie funkcji edit

--- main.py
+++ main.py
@@ -17,11 +17,18 @@ def add():
     todos.append({"task": todo, "done" : False})
     return redirect(url_for("index"))
 
-
+@app.route("/edit/<int:index>", methods=["GET", "POST"])
 def edit(index):
     todo = todos[index]
     if request.method == "POST":
         todo['task'] == request.form["todo"]
+        return redirect((url_for("index")))
+    else:
+        return render_template("edit.html", todo=todo, index=index)
+
+
+
+
 
 
 if __name__ == '__main__':
